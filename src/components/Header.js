38fb22453b5347ff7babf5ddf5b612d52fff08f8
Jesus Soto <sotous0@gmail.com>
2025-10-04 17:25:58 -0500

    Remove identation from header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
     return (
       <div>
         <h1 className='f1'>RoboFriends</h1>
-                <CounterButton />
+        <CounterButton />
       </div>
     );
   }
